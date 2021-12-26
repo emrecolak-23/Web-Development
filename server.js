afcd430e9ffe98a18ce5153b156f2f839863556b
Emre Çolak <colakkemre@gmail.com>
2021-12-26 12:44:46 +0300

    Server Dosyası Yenilendi

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const http = require('http');
 const fs = require('fs');
+const index = fs.readFileSync("index.html")
 
 const requestListener = function (req,res) {
 
   res.writeHead(200);
-  const index = fs.readFileSync("index.html")
-  res.end(index.toString());
+  if (req.url == "/json") {
+    const json = fs.readFileSync("./data.json")
+    res.end(json.toString());
+  } else {
+    const index = fs.readFileSync("index.html")
+    res.end(index.toString());
+
+  }
   
 
 }
