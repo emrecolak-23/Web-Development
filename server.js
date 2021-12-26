249cafe6a76f82c245e16f8d2543ecfab72f2163
Emre Çolak <colakkemre@gmail.com>
2021-12-26 12:31:47 +0300

    Index HTML Gösterildi

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require('http');
-
+const fs = require('fs');
 
 const requestListener = function (req,res) {
 
   res.writeHead(200);
-  
-  res.end("Emre ÇOLAK");
+  const index = fs.readFileSync("index.html")
+  res.end(index.toString());
   
 
 }
